086a8192186d10169d8d6e47549a29ee81b3982e
Pau Ramon Revilla <masylum@gmail.com>
2023-06-09 16:47:27 +0200

    Add support for python

--- private_dot_config/nvim/lua/lsp/clients/pyright.lua
+++ private_dot_config/nvim/lua/lsp/clients/pyright.lua
@@ -2,7 +2,7 @@ local M = {}
 local lspconfig = require("lspconfig")
 
 M.setup = function(on_attach, capabilities)
-    lspconfig.solargraph.setup({
+    lspconfig.pyright.setup({
         on_attach = on_attach,
         capabilities = capabilities,
     })
--- private_dot_config/nvim/lua/plugins/which-key.lua
+++ private_dot_config/nvim/lua/plugins/which-key.lua
@@ -1,48 +1,48 @@
 local keymap = vim.keymap.set
 require("which-key").setup({
     plugins = {
-        marks = true, -- shows a list of your marks on ' and `
+        marks = true,     -- shows a list of your marks on ' and `
         registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
         presets = {
-            operators = false, -- adds help for operators like d, y, ...
-            motions = false, -- adds help for motions
+            operators = false,    -- adds help for operators like d, y, ...
+            motions = false,      -- adds help for motions
             text_objects = false, -- help for text objects triggered after entering an operator
-            windows = true, -- default bindings on <c-w>
-            nav = true, -- misc bindings to work with windows
-            z = true, -- bindings for folds, spelling and others prefixed with z
-            g = true, -- bindings for prefixed with g
+            windows = true,       -- default bindings on <c-w>
+            nav = true,           -- misc bindings to work with windows
+            z = true,             -- bindings for folds, spelling and others prefixed with z
+            g = true,             -- bindings for prefixed with g
         },
     },
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
         separator = "➜", -- symbol used between a key and it's label
-        group = "+", -- symbol prepended to a group
+        group = "+",      -- symbol prepended to a group
     },
     window = {
-        border = "single", -- none, single, double, shadow
-        position = "bottom", -- bottom, top
-        margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
+        border = "single",        -- none, single, double, shadow
+        position = "bottom",      -- bottom, top
+        margin = { 0, 0, 0, 0 },  -- extra window margin [top, right, bottom, left]
         padding = { 0, 0, 0, 0 }, -- extra window padding [top, right, bottom, left]
     },
     layout = {
-        height = { min = 2, max = 25 }, -- min and max height of the columns
-        width = { min = 10, max = 70 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
+        height = { min = 2, max = 25 },                                           -- min and max height of the columns
+        width = { min = 10, max = 70 },                                           -- min and max width of the columns
+        spacing = 3,                                                              -- spacing between columns
     },
     hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-    show_help = true, -- show help message on the command line when the popup is visible
+    show_help = true,                                                             -- show help message on the command line when the popup is visible
 })
 
 keymap("n", "<Space>", "<NOP>", { silent = true })
 vim.g.mapleader = " "
 
 local opts = {
-    mode = "n", -- NORMAL mode
+    mode = "n",     -- NORMAL mode
     prefix = "<leader>",
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true, -- use `silent` when creating keymaps
+    buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true,  -- use `silent` when creating keymaps
     noremap = true, -- use `noremap` when creating keymaps
     nowait = false, -- use `nowait` when creating keymaps
 }
