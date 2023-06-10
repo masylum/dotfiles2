1151b41c9d8c5a1547a837f61841face0e0b4aca
Pau Ramon Revilla <masylum@gmail.com>
2023-06-10 15:12:26 +0200

    even simpler git

--- private_dot_config/bash/prompt.bash
+++ private_dot_config/bash/prompt.bash
@@ -1,10 +1,6 @@
-parse_git_branch() {
-   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
-
 _RESET="\e[0m"
 _YELLOW="\e[33m"
 _BLUE="\e[34m"
 _GREEN="\e[32m"
 
-PS1="${_GREEN}\w${_BLUE}$(parse_git_branch) ${_YELLOW}\$ ${_RESET}"
+PS1="${_GREEN}\w${_BLUE}$(__git_ps1) ${_YELLOW}\$ ${_RESET}"
