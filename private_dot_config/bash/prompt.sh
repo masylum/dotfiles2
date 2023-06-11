6f0f2b06690e127f31adc8042e677ced911da7c1
Pau Ramon Revilla <masylum@gmail.com>
2023-06-11 20:05:57 +0200

    asdfa

--- private_dot_config/bash/prompt.sh
+++ private_dot_config/bash/prompt.sh
@@ -1,6 +1,6 @@
-_RESET="\e[0m"
-_YELLOW="\e[33m"
-_BLUE="\e[34m"
-_GREEN="\e[32m"
+_RESET="\[\e[0m\["
+_YELLOW="\[\e[33m\["
+_BLUE="\[\e[34m\["
+_GREEN="\[\e[32m\["
 
 PS1="${_GREEN}\w${_BLUE}\$(__git_ps1 "(%s)") ${_YELLOW}\$ ${_RESET}"
