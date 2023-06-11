49064fad6016f1c924998b682a9b5efa9faed09a
Pau Ramon Revilla <masylum@gmail.com>
2023-06-11 19:51:05 +0200

    baaash

--- private_dot_config/bash/prompt.sh
+++ private_dot_config/bash/prompt.sh
@@ -3,4 +3,4 @@ _YELLOW="\e[33m"
 _BLUE="\e[34m"
 _GREEN="\e[32m"
 
-export PS1="${_GREEN}\w${_BLUE}$(__git_ps1 "(%s)") ${_YELLOW}\$ ${_RESET}"
+export PS1='${_GREEN}\w${_BLUE}$(__git_ps1 '(%s)") ${_YELLOW}\$ ${_RESET}"
