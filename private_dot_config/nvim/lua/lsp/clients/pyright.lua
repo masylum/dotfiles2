c9fb5b25209c7f90462b594647b68f1f5f05efc4
Pau Ramon Revilla <masylum@gmail.com>
2023-06-13 22:54:49 +0200

    support for poetry

--- private_dot_config/nvim/lua/lsp/clients/pyright.lua
+++ private_dot_config/nvim/lua/lsp/clients/pyright.lua
@@ -9,12 +9,11 @@ local function get_python_path(workspace)
     return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
   end
 
-  -- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({ '*', '.*' }) do
-    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
-    if match ~= '' then
-      return path.join(path.dirname(match), 'bin', 'python')
-    end
+  -- Find and use virtualenv via poetry in workspace directory.
+  local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+  if match ~= '' then
+    local venv = vim.fn.trim(vim.fn.system('poetry --directory ' .. workspace .. ' env info -p'))
+    return path.join(venv, 'bin', 'python')
   end
 
   -- Fallback to system Python.
